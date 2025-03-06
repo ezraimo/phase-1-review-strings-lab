0ee038513834585fce83c3219cdb7c59b5142269
EZRA KIPRUTO SEREM <ezraserem4038@gmail.com>
2025-03-06 14:50:12 +0300

    tests pass

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+// Code your solution in this file!
+
+const currentUser = "Grace Hopper"; // You can use any name here
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; // Use template literals
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`; // Uppercase message
+const shortGreeting = `Welcome, ${currentUser[0]}!`; // First initial of the user's name
+
